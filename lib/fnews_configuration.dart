7691b309068311d0b91953bc2273595dec76b7cd
Slavek <jgroman@users.noreply.github.com>
2017-05-18 21:32:13 +0200

    Async config loader

--- lib/fnews_configuration.dart
+++ lib/fnews_configuration.dart
@@ -1,6 +1,29 @@
+import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum ThemeName { light, dark }
+
+Future<FlutterNewsConfiguration> loadConfigFromPrefs() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String themeString = prefs.getString('theme') ?? "light";
+
+  ThemeName tn;
+
+  switch (themeString) {
+    case 'dark':
+      tn = ThemeName.dark;
+      break;
+    case 'light':
+      tn = ThemeName.light;
+      break;
+    default:
+      tn = ThemeName.light;
+      break;
+  }
+
+  return new FlutterNewsConfiguration(themeName: tn);
+}
 
 class FlutterNewsConfiguration {
   FlutterNewsConfiguration({@required this.themeName}) {
@@ -12,4 +35,5 @@ class FlutterNewsConfiguration {
   FlutterNewsConfiguration copyWith({ThemeName themeName}) {
     return new FlutterNewsConfiguration(themeName: themeName ?? this.themeName);
   }
+
 }
